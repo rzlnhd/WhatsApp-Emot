729e85f8719fb11812ca05745b200b3aa8fb0c23
Rizal Nurhidayat <43670693+rzlnhd@users.noreply.github.com>
2019-07-07 16:27:03 +0700

    Patch 1.2.4
    Bug Fixing

--- WhatsApp Emoticon.user.js
+++ WhatsApp Emoticon.user.js
@@ -6,8 +6,8 @@
 // @icon         https://i.imgur.com/K6tyGmQ.png
 // @homepageURL  https://openuserjs.org/scripts/rzlnhd/WhatsApp_Emoticon
 // @supportURL   https://openuserjs.org/scripts/rzlnhd/WhatsApp_Emoticon/issues
-// @version      1.2.3
-// @date         2019-05-029
+// @version      1.2.4
+// @date         2019-07-07
 // @author       Rizal Nurhidayat
 // @match        https://web.whatsapp.com/
 // @grant        none
@@ -21,12 +21,15 @@
 
 /* Global Variables */
 var emoti = [" :)"," :D"," <:3"," <3"," (/)"," :%"," :z"," :p"," :\')"," :\'D"," :L"," :g"," :^"," :v"," :@"," :o"," ^^"],
-	emoji = ["🙂","😁","😍","❤","🙏","👏","✅","😋","😂","🤣","💪","👻","👆","👇","😡","😱","😊"],version = "v1.2.3";
+	emoji = ["🙂","😁","😍","❤","🙏","👏","✅","😋","😂","🤣","💪","👻","👆","👇","😡","😱","😊"],version = "v1.2.4",
+    i_alt = ["❤","☺"],
+    p_def=1;
 /* First Function */
 var timer = setInterval(general,1000);
 function general(){
     if(document.getElementsByClassName("_1uESL")[0] != null){
-		document.addEventListener("input", initListener);
+		document.addEventListener("click", initListener);
+        document.addEventListener("click", getPdef);
 		console.log("WhatsApp Emoticon "+version+" - Free Emoticon!");
 		console.log("Tambahkan spasi sebelum menuliskan emoKey.");
 		clearInterval(timer);
@@ -39,34 +42,44 @@ function initListener(){
 	var obj=document.getElementsByClassName("_3u328")[0];
 	if(obj!=null){obj.addEventListener("input", eEmoji)};
 }
+function getPdef(){
+	var obj=document.getElementsByClassName("_3u328")[0];
+    p_def = getCaretPosition(obj);
+}
 /*=====================================
    Main Function : Change Emoticon to Emoji.
 =====================================*/
 function eEmoji(e){
-	var i,text = this.innerText,pos,j,
-        emo=this.getElementsByTagName("img")[0];
+	var i,text = this.innerText,html = this.innerHTML,pos,j,
+        emo=this.getElementsByTagName("img");
 	for(i=0;i<emoti.length;i++){
 		if(text.includes(emoti[i])){
 			text=text.replace(emoti[i],emoji[i]);
             j=i;
 		}
     }
-	if(this.innerText!=text){
-		pos=getCaretPosition(this)
+    pos=getCaretPosition(this);
+    if((pos!=0 && pos!=p_def) || (pos==0 && text.length==0)){p_def=pos;} else if(pos==0 && text.length!=0){pos=p_def;}
+	if(emo.length!=0){
+        j=-2;
+        for(i=0;i<i_alt.length;i++){
+            if(emo[0].getAttributeNode("alt").value===i_alt[i]){
+                j=-1;
+            }
+        }
+        this.innerText=html.replace(emo[0].outerHTML,emo[0].getAttributeNode("alt").value);
+        setCaretPosition(this,pos,j);
+    } else if(this.innerText!=text){
 		this.innerText=text;
-		setCaretPosition(this,pos,j);
-	} else if(emo!=null){
-        this.innerText+=emo.getAttributeNode("alt").value;
-        eBack(this);
-    }
+        setCaretPosition(this,pos,j);
+	}
 }
 /*=====================================
    Utilities Function : Get and Set Cursor Position
 =====================================*/
 /* Get Cursor Position */
 function getCaretPosition(el) {
-  var caretPos = 0,
-    sel, range;
+  var caretPos = 0, sel, range;
   if (window.getSelection) {
     sel = window.getSelection();
     if (sel.rangeCount) {
@@ -91,7 +104,10 @@ function getCaretPosition(el) {
 /* Set Cursor Position */
 function setCaretPosition(el, p, i){
 	var range,sel;
-	if(i==2||i==4||i==8||i==9){p-=2;} else{p-=1;}
+	if(i==2||i==4||i==8||i==9){p-=2;}
+    else if(i==-1){p+=1;p_def=p;}
+    else if(i==-2){p+=2;p_def=p;}
+    else{p-=1;}
 	if(document.createRange){
         range = document.createRange();
         sel = window.getSelection();
@@ -100,16 +116,4 @@ function setCaretPosition(el, p, i){
 		sel.removeAllRanges();
 		sel.addRange(range);
 	}
-}
-/* Set Cursor At The End */
-function eBack(obj){
-	var range,selection;
-	if(document.createRange){
-        range = document.createRange();
-        range.selectNodeContents(obj);
-        range.collapse(false);
-        selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
 }
